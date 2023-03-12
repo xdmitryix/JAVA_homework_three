20cc2739941f87c9314461984f4652c10766ff74
dmitry <rbdeman@yandex.ru>
2023-03-12 14:12:36 +0300

    создал файл второй задачи

---
+++ task_two.java
@@ -0,0 +1,8 @@
+// Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее ариф. из этого списка.
+
+public class task_two {
+
+    public static void main(String[] args) {
+        
+    }
+}
